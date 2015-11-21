67c631c2a719a74f8628678d58ef06cd0a72b247
Luis Borjas <luisfborjas@gmail.com>
2015-11-21 00:00:36 -0600

    Initial Commit

---
+++ Trivial/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Trivial
+//
+//  Created by Luis Borjas on 11/21/15.
+//  Copyright © 2015 Luis & Yamil. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
